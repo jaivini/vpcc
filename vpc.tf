b1d73d9c29073ee00f379d5e2abd0709cf8ab0c7
jaivini <vineetha09982gmail.com>
2023-06-21 11:48:06 +0530

    vpc

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+   tags = {
+    Name = "automated-vpc"
+   }
+}
